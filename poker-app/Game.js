cb5ccaceda7a21eb2cbd5b910eb71c3ea5808b60
ivan rouskov <rouskoi@live.com>
2019-07-24 14:09:48 -0700

    basic navigation finally functions

--- poker-app/Game.js
+++ poker-app/Game.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 class GameScreen extends Component {
-    static navigationOptions = {
-        title: "Welcome"
-    };
+    
     render() {     
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent:'center'}}>
--- poker-app/Home.js
+++ poker-app/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class HomeScreen extends Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Welcome to Poker!</Text>
+                <Text>Welcome to Poker</Text>
+                <Button title="Game" onPress={() => this.props.navigation.navigate('Game')} />
+                 
             </View>
         )
     }
