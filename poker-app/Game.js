f6784eabc12b5ddbed757aec616f56e92673e05c
ivan rouskov <rouskoi@live.com>
2019-08-07 16:54:32 -0700

    still storing null for some reason, working on it, reformatted some code

--- poker-app/Game.js
+++ poker-app/Game.js
@@ -6,25 +6,25 @@ class GameScreen extends Component {
     getDataFromController = async (data) => {
         //console.log(data)
         try {
+
             let jsonData = await AsyncStorage.setItem('key', JSON.stringify(data))
-             console.log(`success storing ${jsonData}`);
+            console.log(`success storing ${jsonData}`);
             return jsonData;
         } catch (error) {
             console.log("error saving data");
         }
     }
-    goHome(){
-         this.props.navigation.navigate('Home');
-         console.log()
+    goHome() {
+        this.props.navigation.navigate('Home');
+        console.log()
     }
 
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Controller will go here</Text>
-                <PBC getDataFromController={this.getDataFromController} goHome={this.goHome} /> 
-                <Button title='Go to home screen' onPress={() => this.goHome() }/>
-
+                <PBC getDataFromController={this.getDataFromController} goHome={this.goHome} />
+                <Button title='Go to home screen' onPress={() => this.goHome()} />
             </View>
         )
     }
