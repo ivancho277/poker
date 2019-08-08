4ec7f0de413d30c8d3fdf9221a63a566b00a85a9
ivan rouskov <rouskoi@live.com>
2019-08-08 12:46:11 -0700

    able to retrieve array of saved keys in AsyncStorage

--- poker-app/Game.js
+++ poker-app/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage, TouchableOpacity } from 'react-native';
 import PBC from './components/PracticeButtonController'
 //import Controller from './components/Controller'
 class GameScreen extends Component {
@@ -8,10 +8,15 @@ class GameScreen extends Component {
         try {
             AsyncStorage.setItem('key', JSON.stringify(data))
             console.log(`success storing ${data.calls}`);
-            return jsonData;
+            return data;
         } catch (error) {
             console.log("error saving data");
         }
+    }
+
+    retrieveData = async () => {
+        let keys = await AsyncStorage.getAllKeys()
+        console.log(keys);
     }
     goHome() {
         this.props.navigation.navigate('Home');
@@ -24,6 +29,9 @@ class GameScreen extends Component {
                 <Text>Controller will go here</Text>
                 <PBC getDataFromController={this.getDataFromController} goHome={this.goHome} />
                 <Button title='Go to home screen' onPress={() => this.goHome()} />
+                <TouchableOpacity onPress={this.retrieveData}>
+                    <Text>Get All data from storage</Text>
+                </TouchableOpacity>
             </View>
         )
     }
