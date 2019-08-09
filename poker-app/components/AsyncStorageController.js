54d42b56c8b6b87bac4d64c45c15716532cbb5b1
ivan rouskov <rouskoi@live.com>
2019-08-09 10:43:58 -0700

    outline storage component

---
+++ poker-app/components/AsyncStorageController.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text, AsyncStorage } from 'react-native';
+
+export default class AsyncStorageController extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  saveData(data){
+
+  }
+
+  retrieveData = async() => {
+
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> AsyncStorageController </Text>
+      </View>
+    );
+  }
+}
