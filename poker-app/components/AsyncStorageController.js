938389be42b44e6a0a07fde7c2f1f50fb8318516
ivan rouskov <rouskoi@live.com>
2019-08-09 10:50:02 -0700

    implemented current storage methods into component

--- poker-app/components/AsyncStorageController.js
+++ poker-app/components/AsyncStorageController.js
@@ -9,11 +9,22 @@ export default class AsyncStorageController extends Component {
   }
 
   saveData(data){
-
+    try{
+        AsyncStorage.setItem('key', JSON.stringify(data));
+        console.log(`success storing ${data.calls}`);
+        return data;
+    }
+    catch(error){
+        console.log("error saving data");
+    }
   }
 
   retrieveData = async() => {
-
+    let keys = await AsyncStorage.getAllKeys();
+    console.log(keys);
+    let games = await AsyncStorage.getItem(keys[0]);
+    console.log(JSON.parse(games));
+    return JSON.parse(games);
   }
 
   render() {
