9c59ca8ff6d6091937df75207c7da869824f7095
ivan rouskov <rouskoi@live.com>
2019-08-20 21:00:13 -0700

    fixed saving bug

--- poker-app/components/PracticeButtonController.js
+++ poker-app/components/PracticeButtonController.js
@@ -15,7 +15,7 @@ export default class PracticeButtonController extends Component {
     };
 
     async populateGames() {
-       await  storageController.retrieveData().then((res) => {
+        await storageController.retrieveData().then((res) => {
             let pastGames = JSON.parse(res);
             console.log("populate function");
             let arrayOfgames = [];
@@ -27,11 +27,11 @@ export default class PracticeButtonController extends Component {
             console.log(arrayOfgames)
             console.log(res)
             console.log(pastGames)
-            if (res.games) {
-                this.setState({
-                    gamesArray: arrayOfgames
-                });
-            }
+
+            this.setState({
+                gamesArray: arrayOfgames
+            });
+
         }).catch((error) => {
             alert("populate error");
             throw error;
@@ -41,62 +41,62 @@ export default class PracticeButtonController extends Component {
 
 
 
-componentDidMount() {
-    this.populateGames().then(() => {
-        console.log("LOOK UNDER");
-        console.log(this.state.gamesArray)
-    })
-}
-
-
-saveToTags(tag) {
-    let tagsArray = this.state.tags;
-    tagsArray.push(tag);
-    this.setState({
-        tags: tagsArray
-    })
-};
-
-
-
-toBeSaved() {
-    let date = new Date();
-    let gamesObj = {
-        date: date.getDate(),
-        time: date.getTime(),
-        calls: this.state.calls,
-        folds: this.state.folds,
-        raises: this.state.raises,
-        tags: this.state.tags
+    componentDidMount() {
+        this.populateGames().then(() => {
+            console.log("LOOK UNDER");
+            console.log(this.state.gamesArray)
+        })
     }
-    let gamesarr = this.state.gamesArray;
-    console.log("LOOOK")
-    console.log(gamesarr)
-    gamesarr.push(gamesObj)
-    let saveObj = {
-        version: "1.0.0",
-        games: gamesarr
+
+
+    saveToTags(tag) {
+        let tagsArray = this.state.tags;
+        tagsArray.push(tag);
+        this.setState({
+            tags: tagsArray
+        })
+    };
+
+
+
+    toBeSaved() {
+        let date = new Date();
+        let gamesObj = {
+            date: date.getDate(),
+            time: date.getTime(),
+            calls: this.state.calls,
+            folds: this.state.folds,
+            raises: this.state.raises,
+            tags: this.state.tags
+        }
+        let gamesarr = this.state.gamesArray;
+        console.log("LOOOK")
+        console.log(gamesarr)
+        gamesarr.push(gamesObj)
+        let saveObj = {
+            version: "1.0.0",
+            games: gamesarr
+        }
+        return saveObj;
+    };
+
+    render() {
+        return (
+            <View>
+                <Text> PracticeButtonController </Text>
+                <TextInput
+                    style={{ height: 40, borderColor: "#000000", borderWidth: 1, borderStyle: 'solid' }}
+                    placeholder="Type your tags here"
+                    onChangeText={(tag) => this.setState({ tag })}
+                    value={this.state.tag}
+                />
+                <Button title="save tag" onPress={() => this.saveToTags(this.state.tag)} />
+                <Button title={`call, #${this.state.calls}`} onPress={() => this.setState({ calls: ++this.state.calls })} />
+                <Button title={`fold, #${this.state.folds}`} onPress={() => this.setState({ folds: ++this.state.folds })} />
+                <Button title={`raise, #${this.state.raises}`} onPress={() => this.setState({ raises: ++this.state.raises })} />
+                <Button title='Save Data. End game.' onPress={() => storageController.saveData(this.toBeSaved())} />
+
+            </View>
+        );
     }
-    return saveObj;
-};
-
-render() {
-    return (
-        <View>
-            <Text> PracticeButtonController </Text>
-            <TextInput
-                style={{ height: 40, borderColor: "#000000", borderWidth: 1, borderStyle: 'solid' }}
-                placeholder="Type your tags here"
-                onChangeText={(tag) => this.setState({ tag })}
-                value={this.state.tag}
-            />
-            <Button title="save tag" onPress={() => this.saveToTags(this.state.tag)} />
-            <Button title={`call, #${this.state.calls}`} onPress={() => this.setState({ calls: ++this.state.calls })} />
-            <Button title={`fold, #${this.state.folds}`} onPress={() => this.setState({ folds: ++this.state.folds })} />
-            <Button title={`raise, #${this.state.raises}`} onPress={() => this.setState({ raises: ++this.state.raises })} />
-            <Button title='Save Data. End game.' onPress={() => storageController.saveData(this.toBeSaved())} />
-
-        </View>
-    );
-}
 }
