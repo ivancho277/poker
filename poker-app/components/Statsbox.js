655b6372b599fb43799bc582ec4155cac6b43a59
ivan rouskov <rouskoi@live.com>
2019-09-13 23:34:05 -0700

    nothing addded to styling, just a catch block

--- poker-app/components/Statsbox.js
+++ poker-app/components/Statsbox.js
@@ -37,21 +37,22 @@ export default class Statsbox extends Component {
 
     logTagsTotals(){
         this.props.logTags().then((res) => {
-            console.log("FOUNDDDDD")
+            console.log("FOUNDDDDD");
             console.log(res);
             let obj = {
                 games: res
             }
             let totals = calculation.calculateTotalStats(obj);
-            console.log("calls " + totals.calls)
-            console.log("folds " + totals.folds)
-            console.log("raises " + totals.raises)
+            console.log("calls " + totals.calls);
+            console.log("folds " + totals.folds);
+            console.log("raises " + totals.raises);
             this.setState({
                 gamesObj: obj
             })
-        })
-        console.log("FOUND");  
-        console.log(this.state.gamesObj)  
+        }).catch(err){
+            console.log("error finding tag");
+            throw err;
+        } 
     }
 
 
