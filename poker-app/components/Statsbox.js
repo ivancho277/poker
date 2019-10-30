aa9cc8e128e46851a343b8dc2b563ef1590de23e
ivan rouskov <rouskoi@live.com>
2019-10-29 17:55:40 -0700

    changed statsbox to functional componenet, with a single hook, since most of the state was removed

--- poker-app/components/Statsbox.js
+++ poker-app/components/Statsbox.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Text, View, StyleSheet, ActivityIndicator, Button, ImageBackground } from 'react-native';
 import ToggleSwitch from 'toggle-switch-react-native'
 import { MyContext } from '../stateContext/GlobalState'
@@ -7,17 +7,12 @@ const storage = require("./AsyncStorageController.js");
 const cardImg = require('../images/ace_diamond.jpg');
 
 
-export default class Statsbox extends Component {
+export default function Statsbox(props) {
     //will pull saved information about users stats and display them back in the box.   
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOnPositionStats: false
-        }
-    }
+    const [isOnPositionStats, setPositionStats] = useState(false);
 
 
-    isEmpty(obj) {
+    const isEmpty = (obj) => {
         for (var key in obj) {
             if (obj.hasOwnProperty(key))
                 return false;
@@ -25,31 +20,31 @@ export default class Statsbox extends Component {
         return true;
     }
 
-    onToggle(isOn) {
+    const onToggle = (isOn) => {
         console.log("Changed to " + isOn);
     }
 
-    render() {
-        return (
-            <View style={{ height: this.props.height, color: '#32CD32', width: this.props.width, borderColor: '#000000', borderWidth: 3, borderStyle: 'solid' }}>
 
-                <MyContext.Consumer>
-                    {(context) => <ImageBackground style={{ flex: 1, height: '100%', width: '100%' }} source={cardImg}>
-                        {this.props.loading
+    return (
+        <View style={{ height: props.height, color: '#32CD32', width: props.width, borderColor: '#000000', borderWidth: 3, borderStyle: 'solid' }}>
+
+            <MyContext.Consumer>
+                {(context) => <ImageBackground style={{ flex: 1, height: '100%', width: '100%' }} source={cardImg}>
+                    {props.loading
+                        ?
+                        <View style={[spinnerStyles.container, spinnerStyles.horizontal]}>
+                            <ActivityIndicator size='small' color='#0000ff' />
+                        </View>
+                        :
+                        isEmpty(context.state.gamesObj) || context.state.gamesObj === [{}]
                             ?
-                            <View style={[spinnerStyles.container, spinnerStyles.horizontal]}>
-                                <ActivityIndicator size='small' color='#0000ff' />
-                            </View>
+                            <Text>Nothing here</Text>
                             :
-                            this.isEmpty(context.state.gamesObj) || context.state.gamesObj === [{}]
+                            isOnPositionStats === true
                                 ?
-                                <Text>Nothing here</Text>
-                                :
-                                this.state.isOnPositionStats === true
-                                    ?
-                                    <Text>
-                                        Stats by position: {'\n'}
-                                        {/* BB: C: {context.state.totals[0].total_calls}, F: {context.state.totals[0].total_folds}, R: {context.state.totals[0].total_raises} {'\n'}
+                                <Text>
+                                    Stats by position: {'\n'}
+                                    {/* BB: C: {context.state.totals[0].total_calls}, F: {context.state.totals[0].total_folds}, R: {context.state.totals[0].total_raises} {'\n'}
                                     SB: C: {context.state.totals[1].total_calls}, F: {context.state.totals[1].total_folds}, R: {context.state.totals[1].total_raises} {'\n'}
                                     D: C: {context.state.totals[2].total_calls}, F: {context.state.totals[2].total_folds}, R: {context.state.totals[2].total_raises} {'\n'}
                                     D+1: C: {context.state.totals[3].total_calls}, F: {context.state.totals[3].total_folds}, R: {context.state.totals[3].total_raises} {'\n'}
@@ -57,42 +52,42 @@ export default class Statsbox extends Component {
                                     D+3: C: {context.state.totals[5].total_calls}, F: {context.state.totals[5].total_folds}, R: {context.state.totals[5].total_raises} {'\n'}
                                     D+4: C: {context.state.totals[6].total_calls}, F: {context.state.totals[6].total_folds}, R: {context.state.totals[6].total_raises} {'\n'}
                                     D+5: C: {context.state.totals[7].total_calls}, F: {context.state.totals[7].total_folds}, R: {context.state.totals[7].total_raises} {'\n'} */}
-                                        Calls: {calculation.getPercentages(context.state.gamesObj).percentCalls}% {'\n'}
-                                        Folds: {calculation.getPercentages(context.state.gamesObj).percentFolds}% {'\n'}
-                                        Raises: {calculation.getPercentages(context.state.gamesObj).percentRaises}% {'\n'}
+                                    Calls: {calculation.getPercentages(context.state.gamesObj).percentCalls}% {'\n'}
+                                    Folds: {calculation.getPercentages(context.state.gamesObj).percentFolds}% {'\n'}
+                                    Raises: {calculation.getPercentages(context.state.gamesObj).percentRaises}% {'\n'}
 
-                                    </Text>
-                                    :
-                                    <Text style={{ justifyContent: 'center' }} >
-                                        Total Stats:{'\n'}
-                                        calls: {calculation.calculateTotalStats(context.state.gamesObj).calls} {'\n'}
-                                        folds: {calculation.calculateTotalStats(context.state.gamesObj).folds}  {'\n'}
-                                        raises: {calculation.calculateTotalStats(context.state.gamesObj).raises} {'\n'}
-                                        tags:
+                                </Text>
+                                :
+                                <Text style={{ justifyContent: 'center' }} >
+                                    Total Stats:{'\n'}
+                                    calls: {calculation.calculateTotalStats(context.state.gamesObj).calls} {'\n'}
+                                    folds: {calculation.calculateTotalStats(context.state.gamesObj).folds}  {'\n'}
+                                    raises: {calculation.calculateTotalStats(context.state.gamesObj).raises} {'\n'}
+                                    tags:
                 </Text>
-                        }
-                        <Button title="log position stats" onPress={() => this.props.logTotalsByPosition()}></Button>
-                        <Button title="search tags" onPress={() => this.props.logTagsTotals()} />
-                        <ToggleSwitch
-                            isOn={this.state.isOnPositionStats}
-                            onColor="green"
-                            offColor="red"
-                            label="Change View"
-                            labelStyle={{ color: "black", fontWeight: "900" }}
-                            size="mediuim"
-                            onToggle={isOnPositionStats => {
-                                this.setState({ isOnPositionStats });
-                                this.onToggle(isOnPositionStats);
-                            }}
-                        />
-                    </ ImageBackground>
                     }
-                </MyContext.Consumer>
+                    <Button title="log position stats" onPress={() => props.logTotalsByPosition()}></Button>
+                    <Button title="search tags" onPress={() => props.logTagsTotals()} />
+                    <ToggleSwitch
+                        isOn={isOnPositionStats}
+                        onColor="green"
+                        offColor="red"
+                        label="Change View"
+                        labelStyle={{ color: "black", fontWeight: "900" }}
+                        size="mediuim"
+                        onToggle={isOnPositionStats => {
+                            setPositionStats(isOnPositionStats);
+                            onToggle(isOnPositionStats);
+                        }}
+                    />
+                </ ImageBackground>
+                }
+            </MyContext.Consumer>
 
-            </View>
+        </View>
 
-        )
-    }
+    )
+
 
 
 
