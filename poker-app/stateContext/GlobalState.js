39e3747e9f0355a71120c18407cb3bb626cbbd5e
ivan rouskov <rouskoi@live.com>
2019-12-26 16:28:19 -0800

    still broken

--- poker-app/stateContext/GlobalState.js
+++ poker-app/stateContext/GlobalState.js
@@ -1,246 +1,233 @@
-import React, { useState, useContext, Component } from 'react';
+import React, { useState, useContext, Component } from "react";
 //const storage = require('../components/AsyncStorageController.js')
-const calculation = require('../components/statscalculation.js')
-const { saveCurrentGame,
-    retrieveCurrentGame,
-    removeCurrentGame,
-    retrieveData,
-    removeData,
-    saveTags,
-    retrieveTags,
-    removeTags,
-    saveActions,
-    retrieveActions,
-    resetActions,
-    firstTimeLauching
-} = require('../components/AsyncStorageController.js')
-const { isValidTag, validActionAdd, validActionRemove } = require('../utils/validators.js')
+const calculation = require("../components/statscalculation.js");
+const {
+  saveCurrentGame,
+  retrieveCurrentGame,
+  removeCurrentGame,
+  retrieveData,
+  removeData,
+  saveTags,
+  retrieveTags,
+  removeTags,
+  saveActions,
+  retrieveActions,
+  resetActions,
+  firstTimeLauching
+} = require("../components/AsyncStorageController.js");
+const {
+  isValidTag,
+  validActionAdd,
+  validActionRemove
+} = require("../utils/validators.js");
 export const MyContext = React.createContext();
 
-
 export class GlobalState extends Component {
-    state = {
-        position: 0,
-        totalsByPosition: {},
-        totals: {},
-        gamesObj: {},
-        gamesArray: [],
-        allTags: [],
-        totalGames: 0,
-        actions: [],
-        currentGame: {}
+  state = {
+    position: 0,
+    totalsByPosition: {},
+    totals: {},
+    gamesObj: {},
+    gamesArray: [],
+    allTags: [],
+    totalGames: 0,
+    actions: [],
+    currentGame: {}
+  };
+  componentDidMount() {
+    // storage.removeData()
+    this.getDataFromStorage().then(res => {
+      console.log("global state populated");
+    });
+  }
 
-    }
-    componentDidMount() {
-        // storage.removeData()
-        this.getDataFromStorage().then((res) => {
-            console.log('global state populated')
-            
-        })
-    }
+  async getDataFromStorage() {
+    await this.getAllGames();
+    await this.getActions();
+    await this.getTags();
+    await this.getCurrentGame();
+  }
 
-    async getDataFromStorage() {
-        await this.getAllGames();
-        await this.getActions();
-        await this.getTags();
-        await this.getCurrentGame();
-    }
-
-    getTags = async () => {
-        await retrieveTags().then(res => {
-            if (res != undefined && res != null) {
-                this.setState({ allTags: JSON.parse(res) })
-            }
-        }).catch(err => {
-            console.log('NO TAGS IN STORAGE');
-            
-        })
-    }
-
-    getActions = async () => {
-        await retrieveActions().then((res) => {
-            this.setState({ actions: JSON.parse(res) });
-        }).catch(err => {
-            alert('actions!')
-        })
-    }
-
-    getCurrentGame = async () => {
-        return currentTags = await retrieveCurrentGame().then(res => {
-            if (res) {
-                return JSON.parse(res);
-            }
-            else return res;
-        })
-    }
-
-    getAllGames = async () => {
-        await retrieveData().then((res) => {
-            //console.log(JSON.parse(res));
-            //debugger;
-            if (res != undefined) {
-                let pastGames = JSON.parse(res)
-                console.log("SYNCC ", pastGames)
-                let temp = calculation.calculateByPosition(pastGames)
-                let allGamesArray = [];
-                if (pastGames.games) {
-                    pastGames.games.forEach(game => {
-                        allGamesArray.push(game)
-                    })
-                }
-
-                this.setState({
-                    gamesObj: JSON.parse(res),
-                    loading: false,
-                    totals: temp,
-                    gamesArray: allGamesArray,
-                    totalGames: allGamesArray.length
-                })
-                console.log("THIS IS ASYNC")
-                console.log(pastGames)
-                console.log(this.state.gamesObj)
-            }
-        }).catch((error) => {
-            console.log("HOME SCREEN ERROR");
-            resetActions();
-            throw error;
-        })
-    }
-
-    //TODO: create a function to update global actions, remove access to actions storage elsewhere, use validators
-    updateActions(action) {
-
-    }
-    //TODO: finish writing this funtion, use validators
-    updateTags = (tag) => {
-        if (isValidTag(tag, this.state.tags)) {
-            let updatedTages = this.state.allTags.concat(tag);
-            this.setState({
-                allTags: updatedTags
-            }, () => {
-                saveTags(this.state.allTags)
-            })
+  getTags = async () => {
+    await retrieveTags()
+      .then(res => {
+        if (res != undefined && res != null) {
+          this.setState({ allTags: JSON.parse(res) });
         }
-        
-    }
+      })
+      .catch(err => {
+        console.log("NO TAGS IN STORAGE");
+      });
+  };
 
-    updateGames(newGamesObj) {
-        this.setState({
-            gamesObj: newGamesObj,
-            gamesArray: newGamesObj.games
-        })
-        
-        
-    }
+  getActions = async () => {
+    await retrieveActions()
+      .then(res => {
+        this.setState({ actions: JSON.parse(res) });
+      })
+      .catch(err => {
+        alert("actions!");
+      });
+  };
 
+  getCurrentGame = async () => {
+    return (currentTags = await retrieveCurrentGame().then(res => {
+      if (res) {
+        return JSON.parse(res);
+      } else return res;
+    }));
+  };
 
-    //TODO: handle all storage controll in context, after you finish these funtions implement them in settings screen
-    resetActions = () => {
+  getAllGames = async () => {
+    await retrieveData()
+      .then(res => {
+        //console.log(JSON.parse(res));
+        //debugger;
+        if (res != undefined) {
+          let pastGames = JSON.parse(res);
+          console.log("SYNCC ", pastGames);
+          let temp = calculation.calculateByPosition(pastGames);
+          let allGamesArray = [];
+          if (pastGames.games) {
+            pastGames.games.forEach(game => {
+              allGamesArray.push(game);
+            });
+          }
+
+          this.setState({
+            gamesObj: JSON.parse(res),
+            loading: false,
+            totals: temp,
+            gamesArray: allGamesArray,
+            totalGames: allGamesArray.length
+          });
+          console.log("THIS IS ASYNC");
+          console.log(pastGames);
+          console.log(this.state.gamesObj);
+        }
+      })
+      .catch(error => {
+        console.log("HOME SCREEN ERROR");
         resetActions();
-        console.log('actions reset')
+        throw error;
+      });
+  };
+
+  //TODO: create a function to update global actions, remove access to actions storage elsewhere, use validators
+  updateActions(action) {}
+  //TODO: finish writing this funtion, use validators
+  updateTags = tag => {
+    if (isValidTag(tag, this.state.tags)) {
+      let updatedTages = this.state.allTags.concat(tag);
+      this.setState(
+        {
+          allTags: updatedTags
+        },
+        () => {
+          saveTags(this.state.allTags);
+        }
+      );
     }
+  };
 
-    deleteAllTags = () => {
-        removeTags();
-        
-        
-        
+  updateGames(newGamesObj) {
+    this.setState({
+      gamesObj: newGamesObj,
+      gamesArray: newGamesObj.games
+    });
+  }
 
-    }
+  //TODO: handle all storage controll in context, after you finish these funtions implement them in settings screen
+  resetActions = () => {
+    resetActions();
+    console.log("actions reset");
+  };
 
-    addTag() {
+  deleteAllTags = () => {
+    removeTags();
+    console.log("tags removed");
+  };
 
-    }
+  addTag() {}
 
-    addAction() {
+  addAction() {}
 
-    }
+  componentDidUpdate() {
+    //checks to see if any new tags are added to our list of overall tags, and updates state if so.
+    retrieveTags().then(res => {
+      if (res != undefined && res != null) {
+        if (this.state.allTags.length >= 1) {
+          //debugger;
+          if (this.state.allTags.length !== JSON.parse(res).length) {
+            this.setState({ allTags: JSON.parse(res) });
+          }
+        }
+      }
+    });
+    // storage.retrieveData().then((res) => {
+    //     //console.log(JSON.parse(res));
+    //     //debugger;
+    //     if (res != undefined) {
+    //         let pastGames = JSON.parse(res)
+    //         console.log("SYNCC ", pastGames)
+    //         //let temp = calculation.calculateByPosition(pastGames)
+    //         let allGamesArray = [];
+    //         if (pastGames.games) {
+    //             pastGames.games.forEach(game => {
+    //                 allGamesArray.push(game)
+    //             })
+    //         }
+    //         debugger;
+    // if (allGamesArray.length !== this.state.totalGames) {
+    //    this.getDataFromStorage().then((res) => {
+    //        console.log('Update global storage')
+    //    })
+    // }
+    // }
+  }
+  logTotalsByPosition = () => {
+    console.log(calculation.calculateByPosition(this.state.gamesObj));
+    return calculation.calculateByPosition(this.state.gamesObj);
+  };
 
+  incrementPosition() {
+    this.setState({
+      position: this.state.position + 1
+    });
+  }
 
+  setPosition(position) {
+    this.setState({
+      position: position
+    });
+  }
 
+  getGames() {
+    return this.state.gamesObj;
+  }
 
+  getGamesArray() {
+    return this.state.gamesArray;
+  }
 
-    componentDidUpdate() {
-        //checks to see if any new tags are added to our list of overall tags, and updates state if so.
-        retrieveTags().then(res => {
-            if (res != undefined && res != null) {
-                if (this.state.allTags.length >= 1) {
-                    //debugger;
-                    if (this.state.allTags.length !== JSON.parse(res).length) {
-                        this.setState({ allTags: JSON.parse(res) })
-                    }
-                }
-            }
-        })
-        // storage.retrieveData().then((res) => {
-        //     //console.log(JSON.parse(res));
-        //     //debugger;
-        //     if (res != undefined) {
-        //         let pastGames = JSON.parse(res)
-        //         console.log("SYNCC ", pastGames)
-        //         //let temp = calculation.calculateByPosition(pastGames)
-        //         let allGamesArray = [];
-        //         if (pastGames.games) {
-        //             pastGames.games.forEach(game => {
-        //                 allGamesArray.push(game)
-        //             })
-        //         }
-        //         debugger;
-        // if (allGamesArray.length !== this.state.totalGames) {
-        //    this.getDataFromStorage().then((res) => {
-        //        console.log('Update global storage')
-        //    })
-        // }
-        // }
-
-
-    }
-    logTotalsByPosition = () => {
-        console.log(calculation.calculateByPosition(this.state.gamesObj));
-        return calculation.calculateByPosition(this.state.gamesObj);
-    }
-
-    incrementPosition() {
-        this.setState({
-            position: this.state.position + 1
-        })
-    }
-
-
-
-    setPosition(position) {
-        this.setState({
-            position: position
-        })
-    }
-
-    getGames() {
-        return this.state.gamesObj;
-    }
-
-    getGamesArray() {
-        return this.state.gamesArray;
-    }
-
-    //TODO: remember to add new funtions to context provider
-    render() {
-        return (
-            <MyContext.Provider value={{
-                state: this.state,
-                incrementPosition: () => this.incrementPosition(),
-                setPosition: (position) => this.setPosition(position),
-                remount: () => this.componentDidMount(),
-                updateGames: (gamesObj) => { this.updateGames(gamesObj) },
-                getGames: () => this.getGames(),
-                getGamesArray: () => this.getGamesArray(),
-
-
-            }
-            }>
-                {this.props.children}
-            </MyContext.Provider>
-        )
-    }
-
+  //TODO: remember to add new funtions to context provider
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          incrementPosition: () => this.incrementPosition(),
+          setPosition: position => this.setPosition(position),
+          remount: () => this.componentDidMount(),
+          updateGames: gamesObj => {
+            this.updateGames(gamesObj);
+          },
+          getGames: () => this.getGames(),
+          getGamesArray: () => this.getGamesArray()
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
 }
-
