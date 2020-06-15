6d7dc17f1a7c60f1b60fffd9a363e66274cd3696
ivancho27 <rouskoi@live.com>
2020-06-15 16:25:26 -0700

    still working on it

--- poker-app/components/functionalComponents/RadioTable.js
+++ poker-app/components/functionalComponents/RadioTable.js
@@ -8,31 +8,35 @@ import { Tables } from '../../constants/tables.js'
 
 // { width: 340, height: 200, backgroundColor: 'green', borderRadius: 100, alignItems: 'center', margin: 10, opacity: .5, position: 'relative' }
 export function RadioTable(props) {
-    const [value, setValue] = useState('hi')
+    const [value, setValue] = useState(0)
 
 
     return (
         <GameSubscriber>
             {(state, actions) => (
                 <RadioButton.Group
-                    onValueChange={value => { setValue(value); }}
+                    onValueChange={value => { setValue(value); console.log(value)}}
                     value={value}
                 >
                     <View style={styles.oval}>
 
 
-                        <View style={{ width: '100%', height: '100%', position: 'absolute', flexDirection: 'row', flexWrap: 'wrap' }} >
-                            {Tables.simplePositionsArr.map((position, i) => {
+                        <View style={{ width: '100%', height: '100%', flexWrap: 'wrap' }} >
+                            <RadioButton.Item label={Tables.simplePositionsArr[0][0]} value={0} />
+                            <RadioButton.Item label={Tables.simplePositionsArr[1][1]} value={1} />
+                            <RadioButton.Item label={Tables.simplePositionsArr[2][2]} value={2} />
+                            <RadioButton.Item label={Tables.simplePositionsArr[3][3]} value={3} />
+                            {/* {Tables.simplePositionsArr.map((position, i) => {
                                 return (<View style={{ flex: 0, flexDirection: 'column' }} key={i}>
                                     {console.log('WTF!!!!!!', Object.values(position)[0])}
                                     {console.log('WTF!!!!!!', Object.keys(position)[0])}
                                     <RadioButton.Item style={{ flex: 1, height: 80,  flexDirection: 'column', flexBasis: 75 }} label={Object.values(position)[0]} value={Object.keys(position)[0]} />
                                 </View>
                                 )
-                            })
+                            }) */}
 
 
-                            }
+                            
                         </View>
 
 
