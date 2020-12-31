91f94c8c55efbdc998ae7ef89d6cd6aa546bcb59
ivancho27 <rouskoi@live.com>
2020-12-31 14:25:45 -0800

    at least new table is getting correct data now

--- poker-app/components/testComponents/TestTable.js
+++ poker-app/components/testComponents/TestTable.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import { Table, TableWrapper,Col, Cols, Cell, Header, } from 'react-native-table-component';
+import { Dialog, Portal, Button, } from 'react-native-paper';
+
+const TestDialog = (props) => {
+  const [visible, setVisible] = React.useState(false);
+
+  const hideDialog = () => setVisible(false);
+  const showDialog = () => setVisible(true);
+
+  return (
+    <Portal>
+      <Button onPress={showDialog}>Show text</Button>
+      <Dialog visible={visible} onDismiss={hideDialog}>
+        <Dialog.ScrollArea>
+          <ScrollView contentContainerStyle={{paddingHorizontal: 24}}>
+            <Text>{JSON.stringify(props, null, 2)}</Text>
+          </ScrollView>
+        </Dialog.ScrollArea>
+      </Dialog>
+    </Portal>
+  );
+};
+
+
 
 export default class ExampleFive extends Component {
   constructor(props) {
     super(props);
-    const elementButton = (value, bttnText) => (
+    const elementButton = (value, btnText) => (
       <TouchableOpacity onPress={() => {this._alertIndex(value); this._logProps()}}>
         <View style={styles.btn}>
-          <Text style={styles.btnText}>{JSON.stringify(bttnText, null, 2)}</Text>
+          <Text style={styles.btnText}>{btnText}</Text>
         </View>
       </TouchableOpacity>
     );
@@ -31,13 +54,14 @@ export default class ExampleFive extends Component {
   }
 
   _alertIndex(value) {
-    Alert.alert(`This is column ${value}`);
+    Alert.alert(`This is column ${JSON.stringify(value, null, 2)}`);
   }
 
   render() {
     const state = this.state;
     return (
       <View style={styles.container}>
+        <TestDialog props={this.props}></TestDialog>
         <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1}}>
           {/* Left Wrapper */}
           <TableWrapper style={{width: 80}}>
