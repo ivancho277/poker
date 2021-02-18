4eb3a11646856cccffce81bb0db23c1f243344d5
ivancho27 <rouskoi@live.com>
2021-02-18 15:59:06 -0800

    a bit more outlineing

--- poker-app/components/GameCalculations/LiveTableCalculations.js
+++ poker-app/components/GameCalculations/LiveTableCalculations.js
@@ -1,6 +1,40 @@
 /**
  * This file will calculate our necessary percentage data for display while tracking a game.
  */
+//================================================================
+/**
+ * helper functions
+ */
+//NOTE: need to define where this function will be used. and in what component. So I know exactly what data it will be recieving. 
+
+filterActionsListFromLiveGame = (liveGame) => {
+    let actions = [];
+    liveGame.actions.forEach((action) => {
+      actions.push(action.actionName);
+    })
+    return this.props.gameState.data.actions;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//================================================================
+/**
+ * Export functions under
+ */
 
 const getLiveGameActionList = (game) => {
 
@@ -18,6 +52,17 @@ const historicPercentage = ( game ) => {
 }
 
 const historicPercentageAtPosition = ( game ) => {
+
+
+}
+
+
+
+module.exports = {
+    getLiveGameActionList: getLiveGameActionList,
+    currentGamePercentage: currentGamePercentage,
+    historicPercentage: historicPercentage,
+    historicPercentageAtPosition: historicPercentageAtPosition
 
 
 }
