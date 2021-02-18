d680c10ee079ff66ea890f58d555166768a61982
ivancho27 <rouskoi@live.com>
2021-02-18 15:52:43 -0800

    outline of new functions for new table

---
+++ poker-app/components/GameCalculations/LiveTableCalculations.js
@@ -0,0 +1,23 @@
+/**
+ * This file will calculate our necessary percentage data for display while tracking a game.
+ */
+
+const getLiveGameActionList = (game) => {
+
+
+}
+
+
+const currentGamePercentage = ( game ) => {
+
+}
+
+
+const historicPercentage = ( game ) => {
+
+}
+
+const historicPercentageAtPosition = ( game ) => {
+
+
+}
